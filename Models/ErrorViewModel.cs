3fc20aaf1eb4d0f86e14d32bb2afe1dc3118474a
thehuy2k5 <thehuy114@gmail.com>
2026-06-07 13:56:33 +0700

    Inital project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AI_logistics_optimizer.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
